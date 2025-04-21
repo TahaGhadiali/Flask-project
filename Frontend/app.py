dbc8f92293a623d26104e1f2872ceb06d6926908
TahaGhadiali <taha5atffm@gmail.com>
2025-04-21 12:15:48 +0530

    project files added

---
+++ Frontend/app.py
@@ -0,0 +1,41 @@
+from flask import Flask ,render_template,request,redirect
+from datetime import datetime
+import requests
+
+
+
+BACKEND_URI = 'http://127.0.0.1:9000'
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    day_of_week = datetime.today().strftime('%A') +' '+ datetime.today().strftime('%B') + ' ' + datetime.today().strftime('%d') + ', ' + datetime.today().strftime('%Y')
+    current_time = datetime.now().strftime('%H:%M:%S')
+    print(day_of_week)
+    return render_template('index.html',day_of_week=day_of_week,current_time=current_time)
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    form_data = dict(request.form)
+
+    try:
+        response = requests.post(BACKEND_URI + '/submit', json=form_data)
+
+        if response.ok:
+            return 'Data successfully submited'
+        else:
+            error_message = f"Submission failed: {response.text}"
+            return render_template('form.html', error=error_message, form_data=form_data)
+    except requests.exceptions.RequestException as e:
+        error_message = f"Connection error: {e}"
+        return render_template('form.html', error=error_message, form_data=form_data)
+    
+    
+@app.route('/get_data')
+def view():
+    response = requests.get(BACKEND_URI+'/view')
+    
+    return response.json()
+    
+if __name__ == '__main__':
+     app.run(host='127.0.0.1',port=8000,debug=True)
---
+++ backend/app.py
@@ -0,0 +1,53 @@
+from flask import Flask ,request,jsonify
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+MONGO_URI = os.getenv('MONGO_URI')
+# Create a new client and connect to the server
+client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
+db = client.test
+
+collection = db['flask-tutorial']
+
+
+
+
+app = Flask(__name__)
+
+
+
+
+# @app.route('/time')
+# def time():
+#     current_time = datetime.now().strftime('%H:%M:%S')
+#     return current_time
+@app.route('/submit',methods=['POST'])
+def submit():
+    form_data = dict(request.json)
+    
+    collection.insert_one(form_data)
+    
+    return 'Data submitted successfully!'
+
+
+@app.route('/view')
+def view():
+    data = collection.find()
+    
+    data = list(data)
+    
+    for items in data:
+        print(items)
+        
+        del items['_id']
+        
+    data = {
+        'data':data
+    }
+    return jsonify(data)
+    
+if __name__ == '__main__':
+     app.run(host='127.0.0.1',port=9000,debug=True)
